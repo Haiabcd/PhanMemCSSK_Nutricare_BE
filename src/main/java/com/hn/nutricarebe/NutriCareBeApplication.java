8d1d6d2868f5ad273a2e8355bdc7505a91c3d515
Knguyen2003 <nguyenphan1122k3@gmail.com>
2025-08-27 14:31:47 +0700

    init project

---
+++ src/main/java/com/hn/nutricarebe/NutriCareBeApplication.java
@@ -0,0 +1,13 @@
+package com.hn.nutricarebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriCareBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutriCareBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hn/nutricarebe/NutriCareBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hn.nutricarebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriCareBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
